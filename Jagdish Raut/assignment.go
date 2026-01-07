0b10a4efc6ebe53b6fa8048cca544f54cc3ba0ff
jagdish-josh <jagdish.raut@joshsoftware.com>
2026-01-07 20:51:22 +0530

    Added a fibonacci program

--- Jagdish Raut/assignment.go
+++ Jagdish Raut/assignment.go
@@ -18,16 +18,31 @@ return reverse_str
 
 }
 
+func fibonacci (num int) int{
+	if num == 1 || num == 0{
+		return num
+	}
+
+	return fibonacci(num-1) + fibonacci(num-2)
+}
+
 
 
 func main(){
 	
 
 	var str string
+	var num int
 	fmt.Print("Enter a string :")
 
 	fmt.Scan(&str)
 
 	fmt.Println("reverse of string is :", reverse(str))
+
+	fmt.Println("Enter number: ")
+	fmt.Scan(&num)
 	
+	fmt.Println("the",num ,"th fibonacci number is", fibonacci(num))
+	
+
 }
